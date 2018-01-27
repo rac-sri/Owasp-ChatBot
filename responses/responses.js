413ada9ff8ca85c629a6b52bf7fc9e92b5dd3c4e
Rania Zyane <rania.zyane@gmail.com>
2018-01-27 23:20:57 +0000

    write bot responses

---
+++ responses/responses.js
@@ -0,0 +1,24 @@
+module.exports = {
+	whichLanguage: {
+		"text" : "In which language would you like to translate this sentence ?",
+		"quick_replies":[
+	      {
+	        "content_type":"text",
+	        "title":"French",
+	        "payload":"fr"
+	      },
+	      {
+	        "content_type":"text",
+	        "title":"Spanish",
+	        "payload":"es"
+	      },
+	      {
+	        "content_type":"text",
+	        "title":"Dutch",
+	        "payload":"nl"
+	      }
+	    ]
+	}, 
+	helloMessage: "Hey. I'm Translate It. I can help you translate sentences to your favourite langs.\nDon't worry about spelling errors ;)",
+	help: "Please send something similar to:\n\"translate i want to speak French\""
+}
